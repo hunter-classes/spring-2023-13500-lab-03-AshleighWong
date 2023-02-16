89bffcd0a8c86fb3e198bff6c7600802c4746c09
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2023-02-15 20:02:12 -0500

    Made tests for Task A,B,C

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
-// add your code as appropriate 
+//Spring 2023 - Lab 03 - main.cpp
+//Last name: Wong
+//First name: Ashleigh
+//GitHub username: AshleighWong
+
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include "reservoir.h"
+#include "reverseorder.h"
+#include <string>
 
-int main()
+int main() 
 {
+  //Task A tests
+  std::cout << get_east_storage("01/14/2018") << " billion gallons" << "\n";
+  std::cout << get_east_storage("05/26/2018") << " billion gallons" << "\n";
+  std::cout << get_east_storage("08/25/2018") << " billion gallons" << "\n";
+  std::cout << get_east_storage("12/02/2018") << " billion gallons" << "\n";
+  std::cout << "\n\n";
   
+  //Task B Test
+  std::cout << "Minimum storage in East Basin: " << get_min_east() << "\n";
+  std::cout << "Maximum storage in East Basin: " << get_max_east() << "\n";
+  std::cout << "\n\n";
 
+  //Task C Test
+  std::cout << compare_basins("01/08/2018") << " basin was higher" << "\n";
+  std::cout << compare_basins("06/18/2018") << " basin was higher" << "\n";
+  std::cout << compare_basins("04/05/2018") << " basin was higher" << "\n";
+  std::cout << compare_basins("01/24/2018") << " basin was higher" << "\n";
+  std::cout << "\n\n";
+  
+  //Task D Test
   
   return 0;
 }
