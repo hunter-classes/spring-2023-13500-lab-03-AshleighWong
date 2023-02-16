2d234bb3dfd9557a0750409ec5b657ae0b11af60
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2023-02-15 20:02:48 -0500

    added functions

--- reservoir.h
+++ reservoir.h
@@ -1,2 +1,15 @@
-// add your code
+//Spring 2023 - Lab 03 - reservoir.h
+//Last name: Wong
+//First name: Ashleigh
+//GitHub username: AshleighWong
+#pragma once
+#include <string>
 
+//just add the void funcions
+double get_east_storage(std::string date);
+
+double get_min_east();
+
+double get_max_east();
+
+std::string compare_basins(std::string date);
