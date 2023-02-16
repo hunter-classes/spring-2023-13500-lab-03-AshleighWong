961bb5aa8d9e6eadf6f30407282d8e7cc0102765
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2023-02-15 20:06:49 -0500

    added void function

--- reverseorder.h
+++ reverseorder.h
@@ -1,1 +1,9 @@
+//Spring 2023 - Lab 03 - reverseorder.h
+//Last name: Wong
+//First name: Ashleigh
+//GitHub username: AshleighWong
+#pragma once
+#include <string>
 
+//just add the void functions
+void reverse_oreder(std::string date1, std::string date2);
